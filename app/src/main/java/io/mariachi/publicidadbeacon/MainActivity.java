193842758a1d8b8b8f620a72a6608b0db806a298
Asalmerontkd <psrantonio@gmail.com>
2016-11-29 01:49:36 -0600

    Second commit

--- app/src/main/java/io/mariachi/publicidadbeacon/MainActivity.java
+++ app/src/main/java/io/mariachi/publicidadbeacon/MainActivity.java
@@ -3,14 +3,25 @@ package io.mariachi.publicidadbeacon;
 import android.bluetooth.BluetoothAdapter;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.RemoteException;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import org.altbeacon.beacon.Beacon;
 import org.altbeacon.beacon.BeaconConsumer;
+import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.BeaconParser;
+import org.altbeacon.beacon.MonitorNotifier;
+import org.altbeacon.beacon.RangeNotifier;
+import org.altbeacon.beacon.Region;
+
+import java.util.Collection;
 
 public class MainActivity extends AppCompatActivity implements BeaconConsumer {
     BluetoothAdapter bluetoothAdapter;
+    BeaconManager beaconManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,21 +75,93 @@ public class MainActivity extends AppCompatActivity implements BeaconConsumer {
             activaBT.show();
         }
 
-
+        beaconManager = BeaconManager.getInstanceForApplication(this);
+        beaconManager.getBeaconParsers().add(new BeaconParser().setBeaconLayout("m:2-3=0215,i:4-19,i:20-21,i:22-23,p:24-24,d:25-25"));
+        beaconManager.setBackgroundScanPeriod(1100l);
+        beaconManager.setBackgroundBetweenScanPeriod(30000l);
+        beaconManager.bind(this);
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        beaconManager.unbind(this);
     }
 
     @Override
     public void onBeaconServiceConnect() {
+        Region region = new Region("myBeaons", null, null, null);
 
+        //Monitorea en la region por beacons
+        beaconManager.setMonitorNotifier(new MonitorNotifier() {
+            @Override
+            public void didEnterRegion(Region region) {
+                try {
+                    beaconManager.startRangingBeaconsInRegion(region);
+                } catch (RemoteException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void didExitRegion(Region region) {
+                try {
+                    beaconManager.stopRangingBeaconsInRegion(region);
+                } catch (RemoteException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void didDetermineStateForRegion(int i, Region region) {
+
+            }
+        });
+
+        //Cuando encuentra un beacon comienza a obtener sus datos como la distancia
+        beaconManager.setRangeNotifier(new RangeNotifier() {
+            @Override
+            public void didRangeBeaconsInRegion(Collection<Beacon> collection, Region region) {
+                for (final Beacon beacon : collection)
+                {
+                    double precision;
+                    double ratio = (beacon.getRssi()*1.0)/beacon.getTxPower();
+                    if (ratio<1.0)
+                    {
+                        precision=Math.pow(ratio, 10);
+                    }
+                    else
+                    {
+                        precision=(0.89976)*Math.pow(ratio,7.7095) + 0.111;
+                    }
+
+                    if (precision<1) //SI se encuentra a menos de X metros se dispara la notificacion
+                    {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                launchPub(beacon.getBluetoothAddress());
+                            }
+                        });
+                    }
+                }
+            }
+        });
+
+        try
+        {
+            beaconManager.startMonitoringBeaconsInRegion(region);
+        }
+        catch (RemoteException ex)
+        {
+            ex.printStackTrace();
+        }
     }
 
     public void launchPub(String mac)
     {
-
+        Log.d("Beacon",mac);
+        //01:17:C5:55:D2:F1
+        //01:17:C5:58:3C:89
     }
 }
